b7368a5e44b024af656cf6f1aa084ccb06eb7178
Uche Odinamba <uchay2x@icloud.com>
2023-07-18 13:23:23 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Services from "./components/Services.jsx";
+import Meeting from "./components/Meeting.jsx";
+import Projects from "./components/Projects.jsx";
+import ImageSlider from "./components/ImageSlider.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Meeting />
+      <Projects />
+      <ImageSlider />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import {
+  AiFillHome,
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiFillQuestionCircle,
+} from "react-icons/ai";
+import { PiInfinityBold } from "react-icons/pi";
+import { AiOutlineSearch } from "react-icons/ai";
+
+import {
+  MdSupervisedUserCircle,
+  MdCategory,
+  MdBusinessCenter,
+} from "react-icons/md";
+
+function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  return (
+    <div className="bg-gray-900 text-white flex justify-between items-center h-20 px-6 fixed top-0 left-0 right-0 z-10 lg:px-16 ">
+      {/* Hamburger Menu */}
+      <div onClick={() => setNav(!nav)} className="cursor-pointer">
+        <AiOutlineMenu
+          size={30}
+          className="text-white shadow-white shadow-sm hover:scale-105 duration-100"
+        />
+      </div>
+      {/* Chefkim Logo */}
+      <div className="flex justify-center items-center gap-2">
+        <PiInfinityBold size={50} className="" />
+        <h1 className="font-bold text-3xl  ">Infinity</h1>
+      </div>
+      {/* Search Icon and input */}
+      <div className="  rounded-full  shadow-white shadow-sm flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+        <AiOutlineSearch size={25} className="" />
+        <input
+          className="bg-gray-900 p-2 w-full focus:outline-none rounded-full placeholder-gray-200 placeholder:italic"
+          type="text"
+          placeholder="Search the depth..."
+        />
+      </div>
+
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      {nav ? (
+        <div className="bg-gray-900/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      ) : (
+        ""
+      )}
+
+      {/* Side drawer menu */}
+      <div
+        className={
+          nav
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-gray-900 z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
+      >
+        <AiOutlineClose
+          onClick={() => setNav(!nav)}
+          size={30}
+          className="absolute right-4 top-4 cursor-pointer "
+        />
+        <div className="flex justify-start items-center">
+          <h2 className="text-2xl p-4">Infinity</h2>
+          <PiInfinityBold size={20} />
+        </div>
+        <nav>
+          <ul className="flex flex-col p-4 ">
+            <li
+              onClick={() => setNav(!nav)}
+              className="text-xl py-4 px-5 flex cursor-pointer hover:bg-white hover:text-gray-900 hover:rounded-sm"
+            >
+              <div to="/" className="flex justify-between items-center ">
+                <AiFillHome size={25} className="mr-4 " /> Home
+              </div>
+            </li>
+            <li
+              onClick={() => setNav(!nav)}
+              className="text-xl py-4 px-5 flex cursor-pointer hover:bg-white hover:text-gray-900 hover:rounded-sm"
+            >
+              <div to="/menu" className="flex justify-between items-center ">
+                <MdSupervisedUserCircle size={25} className="mr-4" /> About
+              </div>
+            </li>
+            <li
+              onClick={() => setNav(!nav)}
+              className="text-xl py-4 px-5 flex cursor-pointer hover:bg-white hover:text-gray-900 hover:rounded-sm"
+            >
+              <div
+                to="/categories"
+                className="flex justify-between items-center "
+              >
+                <MdCategory size={25} className="mr-4" /> Partners
+              </div>
+            </li>
+            <li
+              onClick={() => setNav(!nav)}
+              className="text-xl py-4 px-5 flex cursor-pointer hover:bg-white hover:text-gray-900 hover:rounded-sm"
+            >
+              <div to="/about" className="flex justify-between items-center ">
+                <MdBusinessCenter size={25} className="mr-4" /> Contact
+              </div>
+            </li>
+            <li
+              onClick={() => setNav(!nav)}
+              className="text-xl py-4 px-5 flex cursor-pointer hover:bg-white hover:text-gray-900 hover:rounded-sm"
+            >
+              <div to="faq" className="flex justify-between items-center ">
+                <AiFillQuestionCircle size={25} className="mr-4" /> FAQ
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
